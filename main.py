b5c54733100c72f8f5486415abed187b027a6331
Luiz Martins <luizmartins.uemg@gmail.com>
2022-07-31 22:18:38 -0300

    Create predict

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import warnings 
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn.svm import SVR 
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 class Pipeline:
     def __init__(self, path:str, kernel:str='poly', timesteps:int=5) -> None:
@@ -46,6 +47,8 @@ class Pipeline:
         #Convert values to float 
         self.df[self.df.columns] = self.df[self.df.columns].astype('float64')
 
+        self.df = self.modeling_for_forecast()
+
         return self.df 
     
     def modeling_for_forecast(self): 
@@ -57,10 +60,29 @@ class Pipeline:
 
     def make_forecast(self):
         self.preprocessing_data()
-        self.modeling_for_forecast()
+
+        df_train = self.df.iloc[:-5]
+        df_test = self.df.iloc[-5:]
+
+        X_train, y_train = df_train[['open', 'high', 'low', 'vol', 'change']], df_train['target']
+        X_test, y_test = df_test[['open', 'high', 'low', 'vol', 'change']], df_test['target']
+        
+        #X_train_scaled, y_train_scaled = MinMaxScaler((-1, 1)).fit_transform(X_train), MinMaxScaler((-1, 1)).fit_transform(y_train.values.reshape(1, -1))
+        #X_test_scaled = MinMaxScaler((-1, 1)).fit_transform(X_test)
+
+        #y_scaler = MinMaxScaler((-1, 1)).fit(y_test.values.reshape(1, -1))
+
+        model = SVR(kernel="linear", C=1.5, epsilon=0.15)
+        model.fit(X_train.values, y_train.values)
+
+        y_pred = model.predict(X_test.values)
+
+        #y_pred_inverse = y_scaler.inverse_transform(y_pred.reshape(1, -1))
+
+        print("\nMSE: ", mean_squared_error(y_test.values, y_pred))
+        print("MAE: ", mean_absolute_error(y_test.values, y_pred))
+        print("\nValores previstos: ", y_pred.reshape(-1), "\n")
 
 pipeline = Pipeline(path='dataset/soybean_bovespa.csv')
 
-df = pipeline.preprocessing_data()
-
-print(df.tail())
+df = pipeline.make_forecast()
